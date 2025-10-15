afc65396efa49c34abca2760acfb2bd9b4ba9829
Hasangit config user.name <hasannarin99@gmail.com>
2025-10-15 21:49:37 +0300

    chore: add minimal electron skeleton + cross-platform build workflow

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('env', {
-  SUPABASE_OAUTH_CLIENT_ID: process.env.SUPABASE_OAUTH_CLIENT_ID || null,
-  SUPABASE_OAUTH_CLIENT_SECRET: process.env.SUPABASE_OAUTH_CLIENT_SECRET || null
-});
